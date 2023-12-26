3d3a27bcedca53dd9c87398a8b4a96f46107bc15
d^a <130265608+doranalytics@users.noreply.github.com>
2023-12-25 17:06:29 -0800

    video and sizing

---
+++ src/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="full-screen">
+      {/* Background video */}
+      <div className="background-video">
+        <video autoPlay loop muted>
+          {/* Make sure to place your video in the public folder and update the path */}
+          <source src="/eacc.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      {/* Button with trapezoid shape */}
+      <a href="#" className="experiment-button">lets accelerate bitches</a>
+    </div>
+  );
+};
+
+export default HomePage;
+
