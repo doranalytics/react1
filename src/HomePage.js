8249b6eb828faf9f176696559e08a65c4183ca5c
d^a <130265608+doranalytics@users.noreply.github.com>
2023-12-25 19:39:53 -0800

    button etc

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
+  const [showChristmasMessage, setShowChristmasMessage] = useState(false);
+
+  const displayChristmasMessage = () => {
+    setShowChristmasMessage(true);
+  };
+
   return (
     <div className="full-screen">
-      {/* Background video */}
       <div className="background-video">
-        <video autoPlay loop muted playsInline>
-          {/* Make sure to place your video in the public folder and update the path */}
+        {/* Background video */}
+        <video autoPlay loop muted playsInline className="video">
           <source src="/eacc.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      {/* Button with trapezoid shape */}
-      <a href="#" className="experiment-button">lets accelerate bitches</a>
-    </div>
+      {/* Button to display Christmas message */}
+      <button onClick={displayChristmasMessage} className="experiment-button">
+        Let's Brainstorm!!!
+      </button>
+      {/* Display the Christmas message if showChristmasMessage is true */}
+      {showChristmasMessage && (
+        <div className="merry-christmas-message">Merry Christmas Retards!</div>
+        )}
+        {/* Text field in the top left corner */}
+        <input type="text" className="top-left-text-field" placeholder="x^Ponent" />
+      </div>
   );
 };
 
 export default HomePage;
-
